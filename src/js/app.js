8cad3d1f0b59bea9917f89d24aefcc2497cee1ab
mokemachine <aidanspektor@gmail.com>
2022-07-01 23:30:40 -0700

    removed some uneccessary code (php, js)

--- src/js/app.js
+++ src/js/app.js
@@ -60,21 +60,7 @@ function validateForm() {
         fadeIn();
         return false;
     }
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("simple-msg").innerHTML = this.responseText;
-            document.forms["myForm"]["name"].value = "";
-            document.forms["myForm"]["email"].value = "";
-            document.forms["myForm"]["subject"].value = "";
-            document.forms["myForm"]["comments"].value = "";
-        }
-    };
-    console.log('WENTTHRU!')
-    xhttp.open("POST", "php/contact.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+    
     return false;
 }
 
